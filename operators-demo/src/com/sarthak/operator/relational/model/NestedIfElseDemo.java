ad838fa0d46ecfae30b30628c6e0e1cba0d3e3e9
dnyaneshs1 <iamdsarode@gmail.com>
2024-12-06 12:49:02 +0530

    NestedIfElse

---
+++ operators-demo/src/com/sarthak/operator/relational/model/NestedIfElseDemo.java
@@ -0,0 +1,46 @@
+package com.sarthak.operator.relational.model;
+
+import java.util.Scanner;
+
+public class NestedIfElseDemo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Any value");
+
+        int no = sc.nextInt();
+
+        if (no >= 0){
+            System.out.println(" No is positive or Zero ");
+
+            if(no %2 == 0){
+
+                System.out.println("No Is Positive And Even");
+
+            if(no > 100){
+
+                System.out.println("No Is Greater then 100 ");
+            }
+            }else{
+
+                System.out.println("No Is Odd");
+            }if (no < 100){
+
+                System.out.println("No Is less the 100");
+
+            }else{
+                System.out.println("No is not less then 100");
+            }
+
+            if(no %5 == 0){
+
+                System.out.println("No is Divisible By 5");
+            }else{
+
+                System.out.println("no is not divisible by 5");
+            }
+        }
+    }
+}
